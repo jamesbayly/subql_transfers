143e312c18e975780f37a3abddfdcc6fc19346e7
James Bayly <46693720+jamesbayly@users.noreply.github.com>
2021-09-08 10:47:47 +1200

    Jamesbayly patch 1 (#1)
    * Update mappingHandlers.ts
    
    * Update

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,28 +1,34 @@
-import {SubstrateExtrinsic,SubstrateEvent,SubstrateBlock} from "@subql/types";
+import {SubstrateEvent} from "@subql/types";
 import {Transfer, Account} from "../types";
 import {Balance} from "@polkadot/types/interfaces";
 
-async function ensureAccounts(accountIds: string[]): Promise<void> {
-    for (const accountId of accountIds) {
-        const account = await Account.get(accountId);
-        if (!account) {
-            await new Account(accountId).save();
-        }
-    }
-}
-
 export async function handleTransfer(event: SubstrateEvent): Promise<void> {
-    const {
-        event: {
-            data: [from, to, amount],
-        },
-    } = event;
-    await ensureAccounts([from.toString(), to.toString()]);
-    const transferInfo = new Transfer(
+    // Get data from the event
+    // The balances.transfer event has the following payload \[from, to, value\]
+    // logger.info(JSON.stringify(event));
+    const from = event.event.data[0];
+    const to = event.event.data[1];
+    const amount = event.event.data[2];
+    // logger.info(from + '' +  to + '' + amount);
+    
+    // ensure that our account entities exist
+    const fromAccount = await Account.get(from.toString());
+    if (!fromAccount) {
+        await new Account(from.toString()).save();
+    }
+    
+    const toAccount = await Account.get(to.toString());
+    if (!toAccount) {
+        await new Account(to.toString()).save();
+    }
+    
+    // Create the new transfer entity
+    const transfer = new Transfer(
         `${event.block.block.header.number.toNumber()}-${event.idx}`,
     );
-    transferInfo.fromId = from.toString();
-    transferInfo.toId = to.toString();
-    transferInfo.amount = (amount as Balance).toBigInt();
-    await transferInfo.save();
+    transfer.blockNumber = event.block.block.header.number.toBigInt();
+    transfer.fromId = from.toString();
+    transfer.toId = to.toString();
+    transfer.amount = (amount as Balance).toBigInt();
+    await transfer.save();
 }
