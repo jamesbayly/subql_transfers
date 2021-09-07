13587d07cd9cb3df6ddfe78fe73f36ef50eab979
James Bayly <bayly.james@gmail.com>
2021-09-08 10:46:45 +1200

    Update

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,16 +1,15 @@
-import {SubstrateExtrinsic,SubstrateEvent,SubstrateBlock} from "@subql/types";
+import {SubstrateEvent} from "@subql/types";
 import {Transfer, Account} from "../types";
 import {Balance} from "@polkadot/types/interfaces";
 
 export async function handleTransfer(event: SubstrateEvent): Promise<void> {
     // Get data from the event
     // The balances.transfer event has the following payload \[from, to, value\]
-    
+    // logger.info(JSON.stringify(event));
     const from = event.event.data[0];
     const to = event.event.data[1];
     const amount = event.event.data[2];
-    
-    logger.info(from, to amount);
+    // logger.info(from + '' +  to + '' + amount);
     
     // ensure that our account entities exist
     const fromAccount = await Account.get(from.toString());
@@ -27,6 +26,7 @@ export async function handleTransfer(event: SubstrateEvent): Promise<void> {
     const transfer = new Transfer(
         `${event.block.block.header.number.toNumber()}-${event.idx}`,
     );
+    transfer.blockNumber = event.block.block.header.number.toBigInt();
     transfer.fromId = from.toString();
     transfer.toId = to.toString();
     transfer.amount = (amount as Balance).toBigInt();
